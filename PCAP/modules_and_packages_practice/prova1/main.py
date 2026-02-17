154f271db62d622336a89a8bac239c65229abb5e
Leonardo <leonardo.pr1512@gmail.com>
2026-02-17 20:36:45 +0100

    esercizi PCAP

---
+++ PCAP/modules_and_packages_practice/prova1/main.py
@@ -0,0 +1,12 @@
+from module import suml, prodl
+
+zeroes = [0 for i in range(5)]
+ones = [1 for i in range(5)] 
+print(suml(zeroes))
+print(prodl(ones))
+
+
+import sys
+
+for p in sys.path:
+    print(p)
---
+++ PCAP/modules_and_packages_practice/prova2/prog/main2.py
@@ -0,0 +1,7 @@
+
+from sys import path
+path.append('..\\packages')
+
+from packages.extra.iota import FunI
+
+print(FunI())
